2ddc38b9f3d664edc322fa9bcd80dd88f2dcc328
Lior <lior2512@gmail.com>
2021-01-31 23:24:16 +0200

    Password Generator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,55 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'pw';
+  password: string;
+  includeLetters: boolean = false;
+  includeNumbers: boolean = false;
+  includeSymbols: boolean = false;
+  length: number = 0;
+
+  onButtonClick(): void {
+    const numbers = '1234567890';
+    const letters = 'abcdefghijklmnopqrstuvwxyz';
+    const symbols = '!@#$%^&*()';
+
+    let validChars = '';
+
+    if (this.includeLetters) {
+      validChars += letters;
+    }
+    if (this.includeNumbers) {
+      validChars += numbers;
+    }
+    if (this.includeSymbols) {
+      validChars += symbols;
+    }
+
+    let generatedPassword = '';
+
+    for (let i = 0; i < this.length; i++) {
+      const index = Math.floor(Math.random() * validChars.length);
+      generatedPassword += validChars[index];
+    }
+
+    this.password = generatedPassword;
+  }
+
+  onChangeUseLetters(): void {
+    this.includeLetters = !this.includeLetters;
+  }
+
+  onChangeUseNumbers(): void {
+    this.includeNumbers = !this.includeNumbers
+  }
+
+  onChangeUseSymbols(): void {
+    this.includeSymbols = !this.includeSymbols
+  }
+
+  onChangeLength(event): void {
+    const parsedValue = parseInt(event.target.value);
+    if (!isNaN(parsedValue)) {
+      this.length = parsedValue;
+    }
+  }
 }
